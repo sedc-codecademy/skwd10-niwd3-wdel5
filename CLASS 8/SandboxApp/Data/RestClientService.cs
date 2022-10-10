487a37198c9f54d28ec2720df362aab660b511ea
Ivan Djikovski <ivan.djikovski@wsa.com>
2022-10-10 20:07:34 +0200

    Class 8 Rest Client

--- CLASS 8/SandboxApp/Data/RestClientService.cs
+++ CLASS 8/SandboxApp/Data/RestClientService.cs
@@ -1,4 +1,7 @@
-﻿namespace SandboxApp.Data
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace SandboxApp.Data
 {
     public class RestClientService
     {
@@ -13,14 +16,79 @@
 
         public async Task<string> GetAsync(string route)
         {
-            HttpResponseMessage response = await _httpClient.GetAsync(route);
-
-            if (response.IsSuccessStatusCode)
+            if (IsConnected())
             {
-                return await response.Content.ReadAsStringAsync();
+                HttpResponseMessage response = await _httpClient.GetAsync(route);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+
+                return response.StatusCode.ToString();
             }
 
-            return response.StatusCode.ToString();
+            return string.Empty;
+        }
+
+        public async Task<string> PostAsync(string route, object body)
+        {
+            if (IsConnected())
+            {
+                var stringContent = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
+
+                var response = await _httpClient.PostAsync(route, stringContent);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+
+                return response.StatusCode.ToString();
+            }
+
+            return string.Empty;
+        }
+
+        public async Task<string> PatchAsync(string route, object body)
+        {
+            if (IsConnected())
+            {
+                var stringContent = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
+
+                var response = await _httpClient.PatchAsync(route, stringContent);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+
+                return response.StatusCode.ToString();
+            }
+
+            return string.Empty;
+        }
+
+        public async Task<string> DeleteAsync(string route)
+        {
+            if (IsConnected())
+            {
+                var response = await _httpClient.DeleteAsync(route);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+
+                return response.StatusCode.ToString();
+            }
+
+            return string.Empty;
+        }
+
+        private bool IsConnected()
+        {
+            return Connectivity.Current.NetworkAccess == NetworkAccess.Internet;
         }
     }
 }
--- CLASS 8/SandboxApp/Data/RestViewModel.cs
+++ CLASS 8/SandboxApp/Data/RestViewModel.cs
@@ -25,5 +25,65 @@ namespace SandboxApp.Data
                 ResultLabel = result;
             });
         }
+
+        [RelayCommand]
+        private async Task GetById()
+        {
+            var result = await _restClientService.GetAsync("posts/1");
+
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                ResultLabel = result;
+            });
+        }
+
+        [RelayCommand]
+        private async Task Post()
+        {
+            var post = new Post
+            {
+                UserId = 10,
+                Id = 101,
+                Title = "My new post",
+                Body  = "My new post body"
+            };
+
+            var result = await _restClientService.PostAsync("posts", post);
+
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                ResultLabel = result;
+            });
+        }
+
+        [RelayCommand]
+        private async Task Patch()
+        {
+            var post = new Post
+            {
+                UserId = 10,
+                Id = 1,
+                Title = "My updated post",
+                Body = "My updated post body"
+            };
+
+            var result = await _restClientService.PatchAsync("posts/1", post);
+
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                ResultLabel = result;
+            });
+        }
+
+        [RelayCommand]
+        private async Task Delete()
+        {
+            var result = await _restClientService.DeleteAsync("posts/2");
+
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                ResultLabel = result;
+            });
+        }
     }
 }
